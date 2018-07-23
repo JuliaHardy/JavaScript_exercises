d60a0b517643d87c5ed1af6a87885457204dfaab
Julia Hardy <julia.joanna.hardy@gmail.com>
2018-07-23 10:43:01 +0200

    Day4

--- Day1/drum.js
+++ Day1/drum.js
@@ -1,6 +1,7 @@
 function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    console.log(e);
     if(!audio) return;// stop
     audio.currentTime = 0;//back to start
     audio.play();
